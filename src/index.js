e9213bb226d41b0f516d313f3a4ffae94e0c00bf
Roman Shevyakov <roman@mgstr.ru>
2023-02-09 16:49:06 +0300

    feat: passing  50 of 50 tests

--- src/index.js
+++ src/index.js
@@ -38,7 +38,28 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const splitToSpaces = expr.split('**********'); //Разделяем всю фразу на массив слов
+    const result = []; //Будущий массив, где слово - подмассив, буква - значение подмассива
+    for (let word of splitToSpaces) { //Делим слова на буквы, собираем буквы в значения подмассива
+        let currentWord = [];
+        for (let i = 1; i <= (word.length / 10); i++) {
+            let dotOrDash = word.toString().slice(((i - 1) * 10), (i * 10));
+            let letterToMorse = ''; //Собираем символы морзе в слова-массивы
+            for (let j = 0; j < 10; j++) {
+                if ((dotOrDash.slice(j, j + 2)) === '10') letterToMorse += '.';
+                else if ((dotOrDash.slice(j, j + 2)) === '11') letterToMorse += '-';
+                j++;
+            }
+            currentWord.push(letterToMorse);
+        }
+        let finalWord = '';
+        for (item of currentWord) {
+            finalWord += MORSE_TABLE[item]
+        }
+        result.push(finalWord);
+    }
+
+    return result.join(' ')
 }
 
 module.exports = {
